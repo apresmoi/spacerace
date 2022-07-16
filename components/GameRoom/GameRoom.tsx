d308e9bdaf1655e1291d1622cd9bc59d235a5b20
GuilleEpayco <guillermo.voleger@payco.co>
2022-07-16 19:25:18 -0300

    feat: adding header and  Icon without conflict

--- components/GameRoom/GameRoom.tsx
+++ components/GameRoom/GameRoom.tsx
@@ -7,8 +7,9 @@ import { Cells } from "./Cells";
 import styles from "./GameRoom.module.scss";
 import { GameUI } from "./GameUI";
 import { Players } from "./Players";
-import { StarsBackground } from "./StarsBackground";
+import { StarsBackground } from "../StarsBackground"
 import { Header} from './components/Header'
+
 
 interface GameRoomProps {
   onNotLoggedIn?: () => void;
@@ -27,7 +28,7 @@ export function GameRoom(props: GameRoomProps) {
   return (
     <div className={styles.gameRoom}>
       <Background />
-      <StarsBackground />
+      <StarsBackground/>
       <Board>
         <Cells />
         <Players />
--- components/Icon/HeaderIcon.tsx
+++ components/Icon/HeaderIcon.tsx
@@ -4,7 +4,7 @@ export function HeaderIcon() {
   return (
     <svg
       width="100%"
-      height="258"
+      height="100"
       viewBox="0 0 4284 258"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
