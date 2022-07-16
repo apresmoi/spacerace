a90d2814052d3eb69e3c40e32962b5039f04c204
Julian Mastieri <43274856+julimastieri@users.noreply.github.com>
2022-07-16 13:29:34 -0300

    feat: choose a room page

---
+++ pages/rooms.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { NextPage } from "next";
+import { Container } from "../components/layout/Container";
+import Image from "next/image";
+import styles from "../styles/Rooms.module.scss";
+import { useRouter } from "next/router";
+import { RoomList } from "../components/RoomList";
+
+const Rooms: NextPage = () => {
+  const router = useRouter();
+  const interval = React.useRef<NodeJS.Timeout>();
+
+  const handleCreateRoom = React.useCallback(() => {
+    router.push("/create-room");
+  }, []);
+
+  return (
+    <Container>
+      <div className={styles.rooms}>
+        <div className={styles.content}>
+          <Image
+            src="https://via.placeholder.com/287x36.png"
+            height={36}
+            width={287}
+            alt="instructions2"
+          />
+          <h1 className={styles.title}>Choose a room</h1>
+          <RoomList />
+          <p>or</p>
+          <button className={styles.button} onClick={handleCreateRoom}>
+            Create New Room
+          </button>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Rooms;
