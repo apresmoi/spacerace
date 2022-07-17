9e40ff5e3d1e3c892f34111f554c685d5a8cf1a7
Juan Cruz Fortunatti <juancruz.fortunatti@leniolabs.com>
2022-07-17 15:58:12 -0300

    fix: fixes

--- components/Icon/Buildings.tsx
+++ components/Icon/Buildings.tsx
@@ -9,7 +9,7 @@ export function Buildings() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_120_1234)">
+      <g clipPath="url(#clip0_120_1234)">
         <path
           d="M318.256 187.864C318.256 187.864 334.741 187.864 334.741 236.016V988.649H342.213V236.016C342.213 187.864 358.698 187.864 358.698 187.864"
           fill="#F8F3DC"
@@ -17,8 +17,8 @@ export function Buildings() {
         <path
           d="M318.256 187.864C318.256 187.864 334.741 187.864 334.741 236.016V988.649H342.213V236.016C342.213 187.864 358.698 187.864 358.698 187.864"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M455 166.755V174.227C455 181.698 404.239 187.747 341.5 187.747C278.761 187.747 228 181.698 228 174.227V166.755"
@@ -27,50 +27,50 @@ export function Buildings() {
         <path
           d="M455 166.755V174.227C455 181.698 404.239 187.747 341.5 187.747C278.761 187.747 228 181.698 228 174.227V166.755"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M341.5 180.275C404.184 180.275 455 174.222 455 166.755C455 159.288 404.184 153.234 341.5 153.234C278.816 153.234 228 159.288 228 166.755C228 174.222 278.816 180.275 341.5 180.275Z"
           fill="#F8F3DC"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M341.5 131.529C404.184 131.529 455 125.476 455 118.009C455 110.542 404.184 104.489 341.5 104.489C278.816 104.489 228 110.542 228 118.009C228 125.476 278.816 131.529 341.5 131.529Z"
           fill="#F8F3DC"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M415.15 165.45C415.15 165.45 407.916 169.126 338.535 169.126C269.035 169.126 262.038 165.45 262.038 165.45L237.369 83.9717H439.819L415.15 165.45Z"
           fill="#FECB2F"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M455 120.145V127.616C455 135.088 404.239 141.137 341.5 141.137C278.761 141.137 228 135.088 228 127.616V120.145C228 120.145 251.246 131.53 341.856 131.53C405.425 131.53 421.318 130.581 455 120.145Z"
           fill="#F8F3DC"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M341.5 89.4274C404.184 89.4274 455 83.3741 455 75.907C455 68.4399 404.184 62.3867 341.5 62.3867C278.816 62.3867 228 68.4399 228 75.907C228 83.3741 278.816 89.4274 341.5 89.4274Z"
           fill="#F8F3DC"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M455 78.041V85.5129C455 92.9846 404.239 99.0332 341.5 99.0332C278.761 99.0332 228 92.9846 228 85.5129V78.041C228 78.041 251.246 89.4266 341.856 89.4266C405.425 89.4266 421.318 88.4778 455 78.041Z"
           fill="#F8F3DC"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M228.119 106.862C228.119 106.862 332.131 126.313 452.628 106.862Z"
@@ -79,57 +79,57 @@ export function Buildings() {
         <path
           d="M228.119 106.862C228.119 106.862 332.131 126.313 452.628 106.862"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
         />
         <path
           d="M260.141 94.7651V126.55"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M282.199 96.8994V128.684"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M304.377 99.0332V130.818"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M326.555 98.5596V130.344"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M348.617 98.5596V130.344"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M370.795 98.5596V130.344"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M392.972 98.5596V130.344"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M415.031 96.6616V128.446"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M244.958 150.744C244.958 150.744 310.662 163.671 432.108 150.744Z"
@@ -138,67 +138,67 @@ export function Buildings() {
         <path
           d="M244.958 150.744C244.958 150.744 310.662 163.671 432.108 150.744"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M280.658 139.12V168.533"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M304.14 141.019V169.245"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M327.743 141.019V169.245"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M351.225 141.019V169.245"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M374.706 141.019V169.245"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M398.311 137.697V167.11"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M384.671 73.1786C384.671 36.6499 365.339 7 341.619 7C317.899 7 298.567 36.6499 298.567 73.1786C298.567 73.1786 311.851 78.6342 341.619 78.6342C371.388 78.6342 384.671 73.1786 384.671 73.1786Z"
           fill="#FA3757"
           stroke="#14192E"
-          stroke-width="2.37199"
-          stroke-miterlimit="10"
+          strokeWidth="2.37199"
+          strokeMiterlimit="10"
         />
         <path
           d="M107.272 724.334L119.877 801.237H134.889L143.953 725.609L107.272 724.334Z"
           fill="#F8F3DC"
           stroke="#14192E"
-          stroke-width="2.8325"
-          stroke-miterlimit="10"
+          strokeWidth="2.8325"
+          strokeMiterlimit="10"
         />
         <path
           d="M31.9268 557.926H242.807C242.807 557.926 194.229 533 137.296 533C137.438 533 57.8442 533 31.9268 557.926Z"
           fill="#F8F3DC"
           stroke="#14192E"
-          stroke-width="2.8325"
-          stroke-miterlimit="10"
+          strokeWidth="2.8325"
+          strokeMiterlimit="10"
         />
         <path
           d="M44.3906 583.56L221.988 587.809L234.452 564.158L44.3906 569.115V583.56Z"
@@ -208,78 +208,78 @@ export function Buildings() {
           d="M220.289 619.249L36.7432 616.983L24.8467 634.403L220.289 628.738V619.249Z"
           fill="#14192E"
           stroke="#FECB2F"
-          stroke-width="2.8325"
-          stroke-miterlimit="10"
+          strokeWidth="2.8325"
+          strokeMiterlimit="10"
         />
         <path
           d="M144.376 610.752C220.247 610.752 281.753 606.377 281.753 600.98C281.753 595.583 220.247 591.208 144.376 591.208C68.5055 591.208 7 595.583 7 600.98C7 606.377 68.5055 610.752 144.376 610.752Z"
           fill="#FA3757"
           stroke="#14192E"
-          stroke-width="2.8325"
-          stroke-miterlimit="10"
+          strokeWidth="2.8325"
+          strokeMiterlimit="10"
         />
         <path
           d="M21.5859 641.91H237.281C237.281 641.91 170.717 676.608 129.08 676.608C129.08 676.608 96.5057 676.608 21.5859 641.91Z"
           fill="#F8F3DC"
           stroke="#14192E"
-          stroke-width="2.8325"
-          stroke-miterlimit="10"
+          strokeWidth="2.8325"
+          strokeMiterlimit="10"
         />
         <path
           d="M121.434 679.723L31.9268 629.021"
           stroke="#14192E"
-          stroke-width="2.8325"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2.8325"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M129.081 676.607L224.112 634.403"
           stroke="#14192E"
-          stroke-width="2.8325"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2.8325"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M121.434 679.723L85.7441 627.747"
           stroke="#14192E"
-          stroke-width="2.8325"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2.8325"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M125.683 679.723L172.702 627.463"
           stroke="#14192E"
-          stroke-width="2.8325"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2.8325"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M125.682 679.723L125.54 618.399"
           stroke="#14192E"
-          stroke-width="2.8325"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2.8325"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M121.433 580.87L217.88 582.852"
           stroke="#14192E"
-          stroke-width="2.8325"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-dasharray="69.38 69.38"
+          strokeWidth="2.8325"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeDasharray="69.38 69.38"
         />
         <path
           d="M69.4551 675.049L125.539 752.376L182.33 676.748L126.247 716.545L69.4551 675.049Z"
           fill="#F8F3DC"
           stroke="#14192E"
-          stroke-width="2.8325"
-          stroke-miterlimit="10"
+          strokeWidth="2.8325"
+          strokeMiterlimit="10"
         />
       </g>
       <defs>
--- components/Icon/SpaceShip.tsx
+++ components/Icon/SpaceShip.tsx
@@ -13,52 +13,52 @@ export function SpaceShip() {
         d="M138.977 67.0669V50.0688L4 57.4976L138.977 67.0669Z"
         fill="#F9F5FF"
         stroke="#182434"
-        stroke-width="2.51823"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2.51823"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M45.4259 67.9481C47.4425 67.9481 49.0773 63.4383 49.0773 57.8752C49.0773 52.312 47.4425 47.8022 45.4259 47.8022C43.4092 47.8022 41.7744 52.312 41.7744 57.8752C41.7744 63.4383 43.4092 67.9481 45.4259 67.9481Z"
         stroke="#F9F5FF"
-        stroke-width="2.51823"
-        stroke-miterlimit="10"
+        strokeWidth="2.51823"
+        strokeMiterlimit="10"
       />
       <path
         d="M63.303 71.9773C66.1541 71.9773 68.4654 65.5509 68.4654 57.6234C68.4654 49.696 66.1541 43.2695 63.303 43.2695C60.4519 43.2695 58.1406 49.696 58.1406 57.6234C58.1406 65.5509 60.4519 71.9773 63.303 71.9773Z"
         stroke="#F9F5FF"
-        stroke-width="2.51823"
-        stroke-miterlimit="10"
+        strokeWidth="2.51823"
+        strokeMiterlimit="10"
       />
       <path
         d="M83.6993 78.273C87.8021 78.273 91.1281 68.9715 91.1281 57.4976C91.1281 46.0236 87.8021 36.7222 83.6993 36.7222C79.5965 36.7222 76.2705 46.0236 76.2705 57.4976C76.2705 68.9715 79.5965 78.273 83.6993 78.273Z"
         stroke="#F9F5FF"
-        stroke-width="2.51823"
-        stroke-miterlimit="10"
+        strokeWidth="2.51823"
+        strokeMiterlimit="10"
       />
       <path
         d="M94.9102 17.584H135.58C135.58 17.584 144.267 85.4503 282.644 60.7716H323.817C323.692 60.7716 144.267 154.324 94.9102 17.584Z"
         fill="#63A5B7"
         stroke="#182434"
-        stroke-width="2.51823"
-        stroke-miterlimit="10"
+        strokeWidth="2.51823"
+        strokeMiterlimit="10"
       />
       <path
         d="M136.08 40.2479C136.08 40.2479 211.123 -10.1168 261.488 7.88857C261.488 7.88857 298.38 21.1093 277.227 67.6966"
         stroke="#F9F5FF"
-        stroke-width="2.51823"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2.51823"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M235.676 58.6309C248.471 58.6309 258.843 48.2583 258.843 35.4631C258.843 22.6679 248.471 12.2954 235.676 12.2954C222.88 12.2954 212.508 22.6679 212.508 35.4631C212.508 48.2583 222.88 58.6309 235.676 58.6309Z"
         fill="#F9F5FF"
         stroke="#182434"
-        stroke-width="2.51823"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2.51823"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M224.974 33.9522C224.974 33.9522 226.359 24.7606 231.018 22.6201C231.018 22.6201 229.003 31.1821 232.151 32.1894C232.151 32.1894 235.299 31.9376 237.691 32.1894C238.824 32.3153 238.069 24.8865 242.979 22.6201C242.979 22.6201 241.972 27.6566 245.12 33.7003C248.897 40.6255 247.638 50.4466 235.047 50.4466C234.921 50.4466 218.553 50.9502 224.974 33.9522Z"
@@ -79,11 +79,11 @@ export function SpaceShip() {
       <path
         d="M241.72 17.8359C241.72 17.8359 250.282 19.9765 252.548 30.1753"
         stroke="#182434"
-        stroke-width="1.25912"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-dasharray="11.33 11.33"
+        strokeWidth="1.25912"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeDasharray="11.33 11.33"
       />
     </svg>
   );
